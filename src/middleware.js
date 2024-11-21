6c65657282198e31358d701b80b0f27188ba94bc
Mastwal Mesfin <126337103+ENDragnee@users.noreply.github.com>
2024-11-21 21:04:35 +0300

    Middleware V2

--- src/middleware.js
+++ src/middleware.js
@@ -5,20 +5,21 @@ export function middleware(request) {
   const userGate = request.cookies.get('userGate'); // Retrieve userGate from cookies
   const url = request.nextUrl.clone();
 
+  
   if (!userId) {
     // Redirect to login page if not logged in
     url.pathname = '/login';
     return NextResponse.redirect(url);
   }
-
+  
   // Protect the gateway page
-  if (url.pathname.startsWith('/gateway') && userGate !== 'gateway') {
+  if (url.pathname.startsWith('/gateway') && userGate === '') {
     url.pathname = '/login'; // Redirect to an unauthorized page if necessary
     return NextResponse.redirect(url);
   }
 
   // Protect the proctor page
-  if (url.pathname.startsWith('/proctor') && userGate !== 'proctor') {
+  if (url.pathname.startsWith('/proctor') && userGate === '') {
     url.pathname = '/login';
     return NextResponse.redirect(url);
   }
