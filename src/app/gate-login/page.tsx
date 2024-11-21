c1f339ad2e90c4f1d37576dedd81777f1a64bb9b
Mastwal Mesfin <126337103+ENDragnee@users.noreply.github.com>
2024-11-21 10:29:14 +0300

    Initial commit v2

---
+++ src/app/gate-login/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import GateLogin from '@/components/gateway-login'
+
+const page = () => {
+  return (
+    <div>
+        <GateLogin/>
+    </div>
+  )
+}
+
+export default page
