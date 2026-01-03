16acc687c40319d9f6cacd2f47f8863e6e9a7e02
Mastwal Mesfin <126337103+ENDragnee@users.noreply.github.com>
2026-01-04 01:16:05 +0300

    feat((student)/layout.tsx): layout init for student

---
+++ src/app/(student)/layout.tsx
@@ -0,0 +1,19 @@
+import { redirect } from "next/navigation";
+import { useSession } from "next-auth/react";
+
+export default async function ProctorLayout({
+  children
+}: {
+  children: React.ReactNode
+}) {
+  const { data: session } = useSession();
+  const user = session?.user;
+
+  if (!user || user.role !== "STUDENT") {
+    redirect("/auth/sign-in");
+  }
+
+  return (
+    children
+  )
+}
