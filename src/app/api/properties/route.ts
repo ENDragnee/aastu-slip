00a0596bc668564e11a7b59069b7ab86ee7ec948
Mastwal Mesfin <126337103+ENDragnee@users.noreply.github.com>
2026-01-10 10:35:24 +0300

    feat(items): fetch properties api

---
+++ src/app/api/properties/route.ts
@@ -0,0 +1,18 @@
+import { NextRequest, NextResponse } from "next/server";
+import { prisma } from "@/lib/prisma";
+
+export async function GET() {
+  try {
+    const properties = await prisma.property.findMany({
+      select: {
+        name: true,
+        description: true,
+      },
+    });
+
+    return NextResponse.json({ properties }, { status: 200 });
+  } catch (error) {
+    console.error("Error getting list of properties", error);
+    return NextResponse.json({ error }, { status: 400 });
+  }
+}
