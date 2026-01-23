72e9cabf7e7deded4e33562549dabc51ab33045f
Mastwal Mesfin <126337103+ENDragnee@users.noreply.github.com>
2026-01-23 22:43:49 +0300

    feat: proctor histories and event

---
+++ src/app/api/histories/proctors/events/route.ts
@@ -0,0 +1,59 @@
+import { NextResponse, NextRequest } from "next/server";
+import { prisma } from "@/lib/prisma";
+import { getApiSession } from "@/lib/server-auth";
+import { Role } from "@/generated/prisma/enums";
+
+export async function GET(request: NextRequest) {
+  try {
+    const session = await getApiSession();
+
+    if (!session || !session.user?.id) {
+      return NextResponse.json(
+        { error: "User is not logged in please login to continue!" },
+        { status: 401 },
+      );
+    }
+    if (
+      session.user.role !== Role.PROCTOR &&
+      session.user.role !== Role.ADMIN
+    ) {
+      return NextResponse.json(
+        { error: "User is not an administrator or a proctor!!" },
+        { status: 401 },
+      );
+    }
+
+    const { searchParams } = request.nextUrl;
+
+    const page = parseInt(searchParams.get("page") || "1");
+    const limit = parseInt(searchParams.get("limit") || "10");
+    const sort = searchParams.get("sort") || "createdAt";
+    const order = searchParams.get("order") || "desc";
+
+    const offset = (page - 1) * limit;
+
+    const histories = await prisma.exitEvent.findMany({
+      where: {
+        exit: {
+          proctorId: session.user.id,
+        },
+      },
+      include: {
+        exit: true,
+      },
+
+      take: limit,
+      skip: offset,
+      orderBy: {
+        exit: {
+          [sort]: order,
+        },
+      },
+    });
+
+    return NextResponse.json(histories, { status: 200 });
+  } catch (err: any) {
+    console.error("Unexpected error: ", err);
+    return NextResponse.json({ error: "Unexpected error" }, { status: 500 });
+  }
+}
