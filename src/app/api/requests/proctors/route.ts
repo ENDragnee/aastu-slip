4757feeaaf35e3cdc8caf683fe15c7b096ca928c
Mastwal Mesfin <126337103+ENDragnee@users.noreply.github.com>
2026-01-24 19:53:45 +0300

    fix: status code to 200

--- src/app/api/requests/proctors/route.ts
+++ src/app/api/requests/proctors/route.ts
@@ -92,7 +92,7 @@ export async function GET(request: NextRequest) {
         [sort]: order,
       },
     });
-    return NextResponse.json(userRequests, { status: 201 });
+    return NextResponse.json(userRequests, { status: 200 });
   } catch (err) {
     return NextResponse.json(
       { error: "Unexpected error: ", err },
