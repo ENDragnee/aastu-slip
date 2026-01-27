2d45b5b57e9ab6b9a3e08fd63df86353d2169675
Mastwal Mesfin <126337103+ENDragnee@users.noreply.github.com>
2026-01-27 09:35:17 +0300

    feat(properties): manage properies by admin

--- src/app/api/properties/route.ts
+++ src/app/api/properties/route.ts
@@ -1,5 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 import { prisma } from "@/lib/prisma";
+import { getApiSession } from "@/lib/server-auth";
+import { Role } from "@/generated/prisma/enums";
 
 export async function GET() {
   try {
@@ -15,5 +17,53 @@ export async function GET() {
   } catch (error) {
     console.error("Error getting list of properties", error);
     return NextResponse.json({ error }, { status: 400 });
+  }
+}
+
+export async function POST(request: NextRequest) {
+  try {
+    const session = await getApiSession();
+
+    if (!session || !session.user?.id) {
+      return NextResponse.json(
+        { error: "User is not logged in please login to continue!" },
+        { status: 401 },
+      );
+    }
+    if (session.user.role !== Role.ADMIN) {
+      return NextResponse.json(
+        { error: "User is not an administrator user!!" },
+        { status: 403 },
+      );
+    }
+
+    const body = await request.json();
+    const { name, description } = {
+      ...body,
+      name: body.name.toLowerCase(),
+      description: body.description.toLowerCase(),
+    };
+
+    if (!name || !description) {
+      return NextResponse.json(
+        { error: "Enter the required fields" },
+        { status: 400 },
+      );
+    }
+
+    const createProperty = await prisma.property.create({
+      data: {
+        name: name,
+        description: description,
+      },
+    });
+
+    return NextResponse.json(createProperty, { status: 201 });
+  } catch (err) {
+    console.error("Unexpected error: ", err);
+    return NextResponse.json(
+      { error: "Unexpected error: ", err },
+      { status: 500 },
+    );
   }
 }
