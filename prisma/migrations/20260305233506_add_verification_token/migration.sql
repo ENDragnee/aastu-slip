51d57164fd226346815a23402101e8694b2673d4
Mastwal Mesfin <126337103+ENDragnee@users.noreply.github.com>
2026-03-06 03:41:05 +0300

    feat: db update verification

---
+++ prisma/migrations/20260305233506_add_verification_token/migration.sql
@@ -0,0 +1,16 @@
+-- CreateTable
+CREATE TABLE "verification_token" (
+    "id" TEXT NOT NULL,
+    "identifier" TEXT NOT NULL,
+    "token" TEXT NOT NULL,
+    "expires" TIMESTAMP(3) NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "verification_token_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "verification_token_token_key" ON "verification_token"("token");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "verification_token_identifier_token_key" ON "verification_token"("identifier", "token");
